679c5163a9068d0d8b72cdd45a00fb557d475144
Taishi Ito <itohlee14@gmail.com>
2022-03-07 01:46:09 +0900

    edit inference

--- scripts/inference.py
+++ scripts/inference.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import time
 import numpy as np
 import torch
+import torchvision
 from PIL import Image
 from torch.utils.data import DataLoader
 import sys
@@ -18,119 +19,86 @@ from utils.common import tensor2im, log_input_image
 from options.test_options import TestOptions
 from models.psp import pSp
 
+from torch.optim import Adam
+
+from criteria import id_loss, w_norm, moco_loss
+
+torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
 def run():
-    test_opts = TestOptions().parse()
+	test_opts = TestOptions().parse()
 
-    if test_opts.resize_factors is not None:
-        assert len(
-            test_opts.resize_factors.split(',')) == 1, "When running inference, provide a single downsampling factor!"
-        out_path_results = os.path.join(test_opts.exp_dir, 'inference_results',
-                                        'downsampling_{}'.format(test_opts.resize_factors))
-        out_path_coupled = os.path.join(test_opts.exp_dir, 'inference_coupled',
-                                        'downsampling_{}'.format(test_opts.resize_factors))
-    else:
-        out_path_results = os.path.join(test_opts.exp_dir, 'inference_results')
-        out_path_coupled = os.path.join(test_opts.exp_dir, 'inference_coupled')
+	if test_opts.resize_factors is not None:
+		assert len(
+			test_opts.resize_factors.split(',')) == 1, "When running inference, provide a single downsampling factor!"
+		out_path_results = os.path.join(test_opts.exp_dir, 'inference_results',
+										'downsampling_{}'.format(test_opts.resize_factors))
+		out_path_coupled = os.path.join(test_opts.exp_dir, 'inference_coupled',
+										'downsampling_{}'.format(test_opts.resize_factors))
+	else:
+		out_path_results = os.path.join(test_opts.exp_dir, 'inference_results')
+		out_path_coupled = os.path.join(test_opts.exp_dir, 'inference_coupled')
 
-    os.makedirs(out_path_results, exist_ok=True)
-    os.makedirs(out_path_coupled, exist_ok=True)
+	os.makedirs(out_path_results, exist_ok=True)
+	os.makedirs(out_path_coupled, exist_ok=True)
 
-    # update test options with options used during training
-    ckpt = torch.load(test_opts.checkpoint_path, map_location='cpu')
-    opts = ckpt['opts']
-    opts.update(vars(test_opts))
-    if 'learn_in_w' not in opts:
-        opts['learn_in_w'] = False
-    if 'output_size' not in opts:
-        opts['output_size'] = 1024
-    opts = Namespace(**opts)
+	# update test options with options used during training
+	ckpt = torch.load(test_opts.checkpoint_path, map_location='cpu')
+	opts = ckpt['opts']
+	opts.update(vars(test_opts))
+	if 'learn_in_w' not in opts:
+		opts['learn_in_w'] = False
+	if 'output_size' not in opts:
+		opts['output_size'] = 1024
+	opts = Namespace(**opts)
 
-    net = pSp(opts)
-    net.eval()
-    net.cuda()
+	net = pSp(opts)
+	net.eval()
+	net.cuda()
 
-    print('Loading dataset for {}'.format(opts.dataset_type))
-    dataset_args = data_configs.DATASETS[opts.dataset_type]
-    transforms_dict = dataset_args['transforms'](opts).get_transforms()
-    dataset = InferenceDataset(root=opts.data_path,
-                               transform=transforms_dict['transform_inference'],
-                               opts=opts)
-    dataloader = DataLoader(dataset,
-                            batch_size=opts.test_batch_size,
-                            shuffle=False,
-                            num_workers=int(opts.test_workers),
-                            drop_last=True)
+	# random input code
+	np.random.seed(seed=2022)
+	# incodes = np.random.randn(1, 512).astype('float32')
+	incodes = np.random.randn(1, 18, 512).astype('float32')
+	optimized_codes = torch.tensor(incodes).float()
+	optimized_codes = torch.nn.Parameter(optimized_codes.data)	
+	optimizer = Adam([optimized_codes], lr=0.05)
 
-    if opts.n_images is None:
-        opts.n_images = len(dataset)
+	# sample image
+	real_img = Image.open(opts.data_path)
+	dataset_args = data_configs.DATASETS[opts.dataset_type]
+	transforms_dict = dataset_args['transforms'](opts).get_transforms()
+	transform_inference = transforms_dict['transform_inference']
+	real_img = transform_inference(real_img).cuda()
 
-    global_i = 0
-    global_time = []
-    for input_batch in tqdm(dataloader):
-        if global_i >= opts.n_images:
-            break
-        with torch.no_grad():
-            input_cuda = input_batch.cuda().float()
-            tic = time.time()
-            result_batch = run_on_batch(input_cuda, net, opts)
-            toc = time.time()
-            global_time.append(toc - tic)
+	max_iter = 10000
+	L1_loss = torch.nn.L1Loss()
+	# ID_loss = id_loss.IDLoss()
 
-        for i in range(opts.test_batch_size):
-            result = tensor2im(result_batch[i])
-            im_path = dataset.paths[global_i]
-
-            if opts.couple_outputs or global_i % 100 == 0:
-                input_im = log_input_image(input_batch[i], opts)
-                resize_amount = (256, 256) if opts.resize_outputs else (opts.output_size, opts.output_size)
-                if opts.resize_factors is not None:
-                    # for super resolution, save the original, down-sampled, and output
-                    source = Image.open(im_path)
-                    res = np.concatenate([np.array(source.resize(resize_amount)),
-                                          np.array(input_im.resize(resize_amount, resample=Image.NEAREST)),
-                                          np.array(result.resize(resize_amount))], axis=1)
-                else:
-                    # otherwise, save the original and output
-                    res = np.concatenate([np.array(input_im.resize(resize_amount)),
-                                          np.array(result.resize(resize_amount))], axis=1)
-                Image.fromarray(res).save(os.path.join(out_path_coupled, os.path.basename(im_path)))
-
-            im_save_path = os.path.join(out_path_results, os.path.basename(im_path))
-            Image.fromarray(np.array(result)).save(im_save_path)
-
-            global_i += 1
-
-    stats_path = os.path.join(opts.exp_dir, 'stats.txt')
-    result_str = 'Runtime {:.4f}+-{:.4f}'.format(np.mean(global_time), np.std(global_time))
-    print(result_str)
-
-    with open(stats_path, 'w') as f:
-        f.write(result_str)
-
-
-def run_on_batch(inputs, net, opts):
-    if opts.latent_mask is None:
-        result_batch = net(inputs, randomize_noise=False, resize=opts.resize_outputs)
-    else:
-        latent_mask = [int(l) for l in opts.latent_mask.split(",")]
-        result_batch = []
-        for image_idx, input_image in enumerate(inputs):
-            # get latent vector to inject into our input image
-            vec_to_inject = np.random.randn(1, 512).astype('float32')
-            _, latent_to_inject = net(torch.from_numpy(vec_to_inject).to("cuda"),
-                                      input_code=True,
-                                      return_latents=True)
-            # get output image with injected style vector
-            res = net(input_image.unsqueeze(0).to("cuda").float(),
-                      latent_mask=latent_mask,
-                      inject_latent=latent_to_inject,
-                      alpha=opts.mix_alpha,
-                      resize=opts.resize_outputs)
-            result_batch.append(res)
-        result_batch = torch.cat(result_batch, dim=0)
-    return result_batch
-
+	# train
+	for iter in range(max_iter):
+		y_hat, _ = net.decoder([optimized_codes],
+								input_is_latent=True,
+								randomize_noise=False)
+		y_hat = net.face_pool(y_hat)
+		y_hat.cuda()
+		result = tensor2im(y_hat[0])
+		z_loss = L1_loss(y_hat[0], real_img)
+		# + ID_loss(result, real_img, optimized_codes)
+		
+		optimizer.zero_grad()
+		z_loss.backward()
+		optimizer.step()
+		
+		if iter % 10 == 0:
+			print(z_loss.item())
+			if iter % 100 == 0:
+				result.save('./results/wplus3/result_{}.png'.format(iter))
+		
+		with open('./results/wplus3/loss.txt', 'a') as f:
+			text = f"{z_loss}\n"
+			f.write(text)
+	result.save('./results/wplus3/result_{}.png'.format(max_iter))
 
 if __name__ == '__main__':
-    run()
+	run()
